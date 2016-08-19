b40cced5fbbc766124a9dfac2536aa335b5a80ef
Pavel Grinchenko <psdcoder@gmail.com>
2016-08-19 16:51:01 +0300

    Added API endpoints on server for username change. Remove test root endpoint in auth

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,16 +1,9 @@
 var Router = require('express').Router;
 
-module.exports = function (app) {
+module.exports = function () {
     var authRoutes = new Router();
 
     authRoutes
-        .get('/', function (req, res) {
-            res.json({
-                host: req.get('host'),
-                url: req.originalUrl,
-                conf: app.get('conf')
-            });
-        })
         .get('/logout', function(req, res){
             req.logout();
             res.redirect('/');
