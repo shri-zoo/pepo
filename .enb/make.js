f05f58357ac349f64b3ad6b2a2794dbb1fad9e90
Aveletsky <ixus.van.axel@gmail.com>
2016-08-20 16:38:24 +0300

    подключена сетка sharps заложен блок описковой строки

--- .enb/make.js
+++ .enb/make.js
@@ -34,11 +34,20 @@ module.exports = function(config) {
 
             // css
             [techs.stylus, {
-                target: '?.css',
+                target: '?.no-grid.css',
                 sourcemap: false,
                 autoprefixer: {
                     browsers: ['ie >= 10', 'last 2 versions', 'opera 12.1', '> 2%']
                 }
+            }],
+
+            [require('sharps').enb, {
+                config: {
+                    maxWidth: '1100px',
+                    gutter: '10px',
+                    flex: 'flex'
+                },
+                source: '?.no-grid.css' // there is the source
             }],
 
             // bemtree
