eb73476e16ec9143ea83fc33d83f08cb97f5aeea
Alexander Malinovskiy <ixus.van.axel@gmail.com>
2016-08-23 16:18:25 +0300

    Базовая верстка страницы поиска

---
+++ common.blocks/searchbar/searchbar.deps.js
@@ -0,0 +1,25 @@
+({
+   shouldDeps: [
+      {
+         block: 'input',
+         mods: {
+            theme: 'islands',
+            size: 'xl',
+            type: 'search',
+            width: 'available'
+         }
+      },
+      {
+         block:'row',
+         mods:{sac:true},
+         elems:[
+            {
+               elem:'col',
+               mods: { mw: 12,sw: 11 }
+            }
+         ]
+      },
+
+
+   ]
+});
