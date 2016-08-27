832655e0b24b4ef097f9dc9f971142166674de9e
Pavel Grinchenko <psdcoder@gmail.com>
2016-08-28 00:34:50 +0300

    Вынес BEMHTML.apply и BEMTREE.apply в хэлпер

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    bem: require('./bem'),
     handleError: require('./handle-error'),
     isValidEntityId: require('./is-valid-entity-id'),
     checkPaginationParams: require('./check-pagination-params')
