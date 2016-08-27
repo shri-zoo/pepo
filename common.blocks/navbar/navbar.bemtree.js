d9aed62a7090364f8f5940f1324bd140f028d1d9
Yury Lunev <yury.lunev@me.com>
2016-08-27 18:07:46 +0300

    Поправлен navbar и выпадающее меню в нём. Добавлена ширина меню в variables.styl

--- common.blocks/navbar/navbar.bemtree.js
+++ common.blocks/navbar/navbar.bemtree.js
@@ -1,101 +1,121 @@
 block('navbar').content()(function () {
     var _this = this;
-    var links = [
-        [
+    var links = {
+        navbar: [
             { type: 'home', url: '/' },
             { type: 'message', url: '/create-message' },
             { type: 'search', url: '/search' },
             { type: 'profile' }
         ],
-        [
-            { type: 'Редактировать профиль', url: '/profile' },
+        dropdown: [
+            { type: 'Профиль', url: '/u/' + _this.data.user.username },
+            { type: 'Настройки', url: '/settings' },
             { type: 'Выход', url: '/api/auth/logout' }
         ]
-    ];
+    };
     var linkSize = 32;
 
     return {
         block: 'row',
-        mods: { svam: true, sac: true },
-        content: links[0].map(function (link) {
-            if (link.type === 'profile') {
+        mods: {
+            svam: true,
+            sac: true
+        },
+        content: links.navbar.map(function (navbarLink) {
+            if (navbarLink.type === 'profile') {
                 return {
-                    block: 'dropdown',
-                    mods: {
-                        switcher: 'link',
-                        theme: 'islands',
-                        size: 'l',
-                        focused: false
-                    },
-                    switcher: {
-                        block: 'link',
-                        mix: [
-                            {
-                                block: 'navbar',
-                                elem: 'link'
-                            },
-                            {
-                                block: 'row',
-                                elem: 'col',
-                                elemMods: { sw: 3 }
-                            }
-                        ],
-                        content: {
-                            block: 'userpic',
-                            width: linkSize,
-                            height: linkSize,
-                            src: _this.data.user.avatar
-                        }
-                    },
-                    popup: {
-                        block: 'popup',
+                    block: 'row',
+                    elem: 'col',
+                    elemMods: { sw: 3 },
+                    content: {
+                        block: 'dropdown',
                         mods: {
-                            autoclosable: true,
+                            switcher: 'link',
+                            theme: 'islands',
+                            size: 'l',
+                            focused: false
                         },
-                        directions: ['bottom-right'],
-                        content: {
-                            block: 'ul',
-                            content: links[1].map(function (l) {
-                                return {
-                                    block: 'button',
-                                    mods: {
-                                        type: 'submit',
-                                        theme: 'islands',
-                                        size: 'xl'
-                                    },
-                                    url: l.url,
-                                    text: l.type
+                        switcher: {
+                            block: 'link',
+                            mix: [
+                                {
+                                    block: 'navbar',
+                                    elem: 'link'
                                 }
-                            })
+                            ],
+                            content: {
+                                block: 'userpic',
+                                width: linkSize,
+                                height: linkSize,
+                                src: _this.data.user.avatar
+                            }
+                        },
+                        popup: {
+                            block: 'popup',
+                            mods: {
+                                autoclosable: true,
+                            },
+                            directions: ['bottom-right'],
+                            content: {
+                                block: 'dropdown-menu',
+                                content: [{
+                                    block: 'user-info-area',
+                                    content: [
+                                        {
+                                            block: 'user-info',
+                                            elem: 'fullname',
+                                            content: _this.data.user.firstName + ' ' + _this.data.user.lastName
+                                        },
+                                        {
+                                            block: 'username',
+                                            content: _this.data.user.username
+                                        },
+                                    ]
+                                },
+                                    links.dropdown.map(function (dropdownLink) {
+                                        return {
+                                            block: 'button',
+                                            mods: {
+                                                type: 'link',
+                                                theme: 'islands',
+                                                size: 'xl'
+                                            },
+                                            url: dropdownLink.url,
+                                            text: dropdownLink.type
+                                        }
+                                    })
+                                ]
+                            }
                         }
                     }
-                };
+                }
+                    ;
             }
 
             return {
-                block: 'link',
-                mix: [
-                    {
-                        block: 'navbar',
-                        elem: 'link'
-                    },
-                    {
-                        block: 'row',
-                        elem: 'col',
-                        elemMods: { sw: 3 }
-                    }
-                ],
-                url: link.url,
+                block: 'row',
+                elem: 'col',
+                elemMods: { sw: 3 },
                 content: {
-                    block: 'icon',
-                    mods: { type: link.type },
-                    mix: {
-                        block: 'navbar',
-                        elem: 'icon'
-                    },
-                    name: link.type,
+                    block: 'link',
+                    mix: [
+                        {
+                            block: 'navbar',
+                            elem: 'link'
+                        }
+                    ],
+                    url: navbarLink.url,
+                    content: {
+                        block: 'icon',
+                        mods: { type: navbarLink.type },
+                        mix: {
+                            block: 'navbar',
+                            elem: 'icon'
+                        },
+                        name: navbarLink.type,
 
-                    size: linkSize
+                        size: linkSize
+                    }
                 }
             }
         })
