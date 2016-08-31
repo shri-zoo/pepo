0461e7fcaca59aa68d3a03debf865e5db1604ced
Pavel Grinchenko <psdcoder@gmail.com>
2016-09-01 00:01:09 +0300

    Закончил логику для страницы настроек пользователя

--- common.blocks/form-settings/form-settings.bemhtml.js
+++ common.blocks/form-settings/form-settings.bemhtml.js
@@ -1,1 +1,13 @@
-block('form-settings').js()(true);
+block('form-settings')(
+    js()(true),
+    elem('avatar-input')(
+        tag()('input'),
+        attrs()(function () {
+            return {
+                type: 'hidden',
+                name: 'avatar',
+                value: this.ctx.value || ''
+            };
+        })
+    )
+);
