e2a9169aec186265655ba615104d217d6ec4a79d
Pavel Grinchenko <psdcoder@gmail.com>
2016-09-04 23:07:25 +0300

    Вынес index страницу в отдельный модификатор

---
+++ common.blocks/page/_view/page_view_index.bemtree.js
@@ -0,0 +1,51 @@
+block('page')
+    .mod('view', 'index')
+    .content()(function () {
+        var onEmpty;
+
+        if (this.data.user.subscribedTo.length) {
+            onEmpty = [
+                'У пользователей на кого вы подписаны нет ни одного сообщения. ',
+                {
+                    block: 'link',
+                    mods: {
+                        theme: 'islands',
+                        size: 'l'
+                    },
+                    url: '/search',
+                    content: 'Найдите еще пользователей!'
+                }
+            ];
+        } else {
+            onEmpty = [
+                'Вы ни на кого не подписаны. ',
+                {
+                    block: 'link',
+                    mods: {
+                        theme: 'islands',
+                        size: 'l'
+                    },
+                    url: '/search',
+                    content: 'Подписаться на пользователей'
+                }
+            ];
+        }
+
+        return {
+            block: 'layout',
+            content: [
+                {
+                    block: 'header'
+                },
+                {
+                    block: 'body',
+                    content: {
+                        block: 'infinite-list',
+                        mods: { type: 'user-messages' },
+                        js: { url: '/messages?html' },
+                        onEmpty: onEmpty
+                    }
+                }
+            ]
+        };
+    });
---
+++ common.blocks/page/_view/page_view_index.deps.js
@@ -0,0 +1,26 @@
+({
+    shouldDeps: [
+        {
+            block: 'layout'
+        },
+        {
+            block: 'header'
+        },
+        {
+            block: 'body'
+        },
+        {
+            block: 'infinite-list',
+            mods: {
+                type: 'user-messages'
+            }
+        },
+        {
+            block: 'link',
+            mods: {
+                theme: 'islands',
+                size: 'l'
+            }
+        }
+    ]
+});
