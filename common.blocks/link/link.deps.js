425a36ebc8acf3f053e82c3e119bafe4755d938a
Pavel Grinchenko <psdcoder@gmail.com>
2016-09-15 00:16:07 +0300

    Добавил кастомные стили для блока link и создал общий блок stylus-mixins

---
+++ common.blocks/link/link.deps.js
@@ -0,0 +1,6 @@
+({
+    mustDeps: [
+        'stylus-vars',
+        'stylus-mixins'
+    ]
+});
---
+++ common.blocks/stylus-mixins/stylus-mixins.deps.js
@@ -0,0 +1,7 @@
+({
+    shouldDeps: [
+        {
+            elems: ['link']
+        }
+    ]
+});
